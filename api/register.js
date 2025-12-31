38288a5bb0f9f2ec3025c0ec5cd86ce9a8df67d0
ivrytb <ytb046@gmail.com>
2025-12-31 16:32:54 +0200

    Update register.js

--- api/register.js
+++ api/register.js
@@ -2,29 +2,27 @@ module.exports = async (req, res) => {
     try {
         const params = { ...req.query, ...req.body };
 
-        // שליפת הנתונים - המערכת תחזיר לנו את user_id בבקשה הבאה כי שירשרנו אותו
         const userId = params.user_id;
         const userAge = params.user_age;
 
         // שלב א: בקשת תעודת זהות
         if (!userId) {
-            // הגדרות: 9 ספרות מינימום ומקסימום, המתנה 10 שניות, סיום בסולמית
-            return res.status(200).send("read=t-נא הקש תעודת זהות ובסיומה סולמית=user_id,no,9,9,10,Digits,yes");
+            // מבנה: שם_פרמטר,השמעת_הקשות,מינימום,מקסימום,סוג,סולמית
+            // דילגנו על 'השמעת הקשות' בעזרת פסיק ריק
+            return res.status(200).send("read=t-נא הקש תעודת זהות ובסיומה סולמית=user_id,,9,9,Digits,yes");
         }
 
-        // שלב ב: יש ת"ז, חסר גיל
+        // שלב ב: בקשת גיל
         if (!userAge) {
-            // כאן התיקון הקריטי: אנחנו מוסיפים את ה-user_id כפרמטר נוסף לתשובה
-            // והגדרנו 1,3,10,Digits,yes (ה-yes בסוף מחייב סולמית ומונע את הפרדוקס)
-            return res.status(200).send(
-                `read=t-נא הקש גיל ובסיומו סולמית=user_age,no,1,3,10,Digits,yes&user_id=${userId}`
-            );
+            // המערכת כבר צברה את user_id ותשלח אותו שוב אוטומטית
+            return res.status(200).send("read=t-נא הקש גיל ובסיומו סולמית=user_age,,1,3,Digits,yes");
         }
 
-        // שלב ג: סיום
-        return res.status(200).send(`id_list_message=t-נרשמת בהצלחה תעודת זהות ${userId} גיל ${userAge}&hangup=yes`);
+        // שלב ג: סיום - קבלת שני הפרמטרים
+        const summary = `נרשמת בהצלחה. תעודת זהות ${userId} וגיל ${userAge} נקלטו`;
+        return res.status(200).send(`id_list_message=t-${summary}&hangup=yes`);
 
     } catch (error) {
-        return res.status(200).send("id_list_message=t-שגיאה");
+        return res.status(200).send("id_list_message=t-חלה שגיאה במערכת");
     }
 };
